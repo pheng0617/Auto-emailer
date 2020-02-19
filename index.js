bc5498cb344621bca948a58dae70c937669d4889
pheng0617 <60480221+pheng0617@users.noreply.github.com>
2020-02-19 18:31:11 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+let cron = require("node-cron");
+let nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+  host: "smtp.googlemail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "pheng617@gmail.com",
+    pass: "Blueprint112"
+  }
+});
+
+let mailOptions = {
+  from: '"Paul SMTP" <pheng617@gmail.com>',
+  to: "pheng0617@gmail.com",
+  subject: "Eat shit and code bitch",
+  text: "Code Code."
+};
+
+cron.schedule("* * * * *", () => {
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+});
